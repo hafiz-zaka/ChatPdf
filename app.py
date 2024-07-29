023f6cd1344a49d608a97b3ae5596e337c357c97
mac <laibanextsalution@gmail.com>
2024-07-29 09:59:36 +0500

    solve indentation error

--- app.py
+++ app.py
@@ -381,7 +381,8 @@ def process_answer(instruction):
         raise
 
 if __name__ == '__main__':
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
 
 
 
